cd90a6bd7c9fc260d8d3b64a6d3f1ce28d3d379a
Reyan Abid <reyanabid20@gmail.com>
2024-10-26 22:07:42 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
+// Locomotive Scroll
 const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true,
-lerp:0.2
+    lerp: 0.2
 });
 
+// Sidebar Menu Toggle
+const menuIcon = document.querySelector('.menu-icon');
+const sidebarMenu = document.getElementById('menu');
 
+menuIcon.addEventListener('click', () => {
+    sidebarMenu.classList.toggle('menu-open');
+});
+
+// Shery.js Effects
 Shery.mouseFollower({
-    //Parameters are optional.
     skew: true,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 0.1,
-    locomotive:true,
+    locomotive: true,
 });
-Shery.makeMagnet(".navbar-section h1" /* Element to target.*/, {
-    //Parameters are optional.
+
+Shery.makeMagnet(".navbar-section h1", {
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
 });
-Shery.textAnimate(".headingh1" /* Element to target.*/, {
-    //Parameters are optional.
+
+Shery.textAnimate(".headingh1", {
     style: 2,
     y: 10,
     delay: 0.1,
@@ -26,6 +34,7 @@ Shery.textAnimate(".headingh1" /* Element to target.*/, {
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     multiplier: 0.1,
 });
-Shery.hoverWithMediaCircle(".headingh1" /* Element to target.*/, {
-    images: ["", "image2.jpg", "image3.jpg"] /*OR*/,
+
+Shery.hoverWithMediaCircle(".headingh1", {
+    images: ["image1.jpg", "image2.jpg", "image3.jpg"],
 });
