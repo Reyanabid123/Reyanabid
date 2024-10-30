eed514650eb2954532fb3e4031945b48f0c9ff3a
Reyan Abid <reyanabid20@gmail.com>
2024-10-30 16:00:40 +0500

    add aniamtion for all heading with shery js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ Shery.mouseFollower({
     locomotive: true,
 });
 
-Shery.makeMagnet(".navbar-section h1", {
+Shery.makeMagnet(".navbar-section h1, .section-4 button", {
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
 });
@@ -36,7 +36,7 @@ Shery.textAnimate(".headingh1", {
     multiplier: 0.1,
 });
 
-Shery.hoverWithMediaCircle(".headingh1", {
+Shery.hoverWithMediaCircle(".headingh1,.project-seen h1, .section-4 h2", {
     images: ["image1.jpg", "image2.jpg", "image3.jpg"],
 
 });
@@ -51,10 +51,5 @@ menuIcon.addEventListener('click', () => {
 Shery.imageEffect(".img-div img", {
     style: 3,
     debug: true,
-    config: {
-        uFrequencyX: { value: 100, range: [0, 100] },
-        uFrequencyY: { value: 44.86, range: [0, 100] },
-        uFrequencyZ: { value: 100, range: [0, 100] },
-        uTime: { value: 37.14299999999965 },
-    },
+   
 });
